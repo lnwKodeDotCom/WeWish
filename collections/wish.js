084c89f4879de1760f8547a417a69d6c0fe2c9f1
zarazi <zarazi@hotmail.com>
2015-12-20 15:47:15 +0700

    Allow insert new wish from client.

--- collections/wish.js
+++ collections/wish.js
@@ -1,2 +1,13 @@
 Wish = new Meteor.Collection('Wish');
 
+Wish.allow({
+  insert: () => true,
+  update: () => false,
+  remove: () => false
+});
+
+Wish.deny({
+  insert: () => false,
+  update: () => true,
+  remove: () => true
+});
