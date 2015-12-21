420c1f5828f1def3e7d58bf41587db194f83eaa1
zarazi <zarazi@hotmail.com>
2015-12-21 16:00:41 +0700

    Added load more wishes feature.

--- client/templates/authenticated/index.js
+++ client/templates/authenticated/index.js
@@ -1,18 +1,34 @@
+const WISHES_STEP = 3;
+
 Template.index.onCreated( () => {
   let template = Template.instance();
+  template.no_of_wishes = new ReactiveVar(WISHES_STEP);
 
   template.autorun(()=> {
-    template.subscribe('wishList');
+    template.subscribe('wishList', template.no_of_wishes.get());
   });
 
 });
 
 Template.index.helpers({
   contentReady() {
-    return Template.instance().subscriptionsReady();
+    var template = Template.instance();
+    return template.no_of_wishes.get()>WISHES_STEP || template.subscriptionsReady();
   },
   wishItems() {
     return Wish.find().fetch();
+  },
+  moreWishes() {
+    var template = Template.instance();
+    return template.no_of_wishes.get() < 15;
+  }
+});
+
+Template.index.events({
+  'click .load-more': (event, template) => {
+    event.preventDefault();
+    template.no_of_wishes.set(template.no_of_wishes.get()+WISHES_STEP);
+    $("html, body").animate({scrollTop: $(".load-more").offset().top}, 500);
   }
 });
 
--- server/publications/wish.js
+++ server/publications/wish.js
@@ -1,5 +1,12 @@
-Meteor.publish('wishList', function() {
-  return Wish.find();
+const MAX_WISHES = 100;
+
+Meteor.publish('wishList', (limit = 5) => {
+  check(limit, Number);
+  const options = {
+    sort: {date_created: -1},
+    limit: Math.min(limit, MAX_WISHES)
+  };
+  return Wish.find({}, options);
 });
 
 Meteor.publish('wishDetail', (id) => {
